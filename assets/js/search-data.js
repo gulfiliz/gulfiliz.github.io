9fa9980f2b64045f00f27e1a4bacc5509305846c
gulfiliz <54915332+gulfiliz@users.noreply.github.com>
2026-04-15 06:29:52 +0000

    Deploying to gh-pages from @ gulfiliz/gulfiliz.github.io@bd724296edaa0a679c0a85fa0b2a2c39d0160d37 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -698,12 +698,7 @@ ninja.data = [{
             window.location.href = "/blog/2024/baslangic/";
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
+      },{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
           section: "News",},{id: "news-a-long-announcement-with-details",
