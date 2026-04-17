9a564915a3d679ca173909e0ec9b9df38c3d584d
gulfiliz <54915332+gulfiliz@users.noreply.github.com>
2026-04-17 03:42:35 +0000

    Deploying to gh-pages from @ gulfiliz/gulfiliz.github.io@61c1327a81a2f6f2650371c09a5ae6d1bd51f695 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -695,7 +695,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2024/tokyo/";
+            window.location.href = "/blog/2024/tokyo-final/";
           
         },
       },{id: "post-tokyo-or-how-i-spent-three-hours-in-a-starbucks-because-i-was-too-scared-to-leave",
@@ -706,7 +706,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2024/tokyo-final/";
+            window.location.href = "/blog/2024/tokyo/";
           
         },
       },{id: "post-beginnings",
