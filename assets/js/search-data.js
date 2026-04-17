a6e8e790214598c3906c201088ab3724155d4f95
gulfiliz <54915332+gulfiliz@users.noreply.github.com>
2026-04-17 04:42:53 +0000

    Deploying to gh-pages from @ gulfiliz/gulfiliz.github.io@220cd299e7bf182068725ea65d5b91fac016af0c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -698,17 +698,6 @@ ninja.data = [{
             window.location.href = "/blog/2024/tokyo-final/";
           
         },
-      },{id: "post-tokyo-or-how-i-spent-three-hours-in-a-starbucks-because-i-was-too-scared-to-leave",
-        
-          title: "Tokyo — Or, How I Spent Three Hours in a Starbucks Because I...",
-        
-        description: "On arriving in Tokyo at 23, speaking zero Japanese, ordering something called &quot;eggslut&quot; for breakfast, and falling irreversibly in love with a city that makes no sense and perfect sense simultaneously.",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/tokyo/";
-          
-        },
       },{id: "post-beginnings",
         
           title: "Beginnings",
