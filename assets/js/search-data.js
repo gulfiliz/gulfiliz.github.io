b86b218e43381fa47dbfeb2a4251810ffe993413
gulfiliz <54915332+gulfiliz@users.noreply.github.com>
2026-06-05 00:00:42 +0000

    Deploying to gh-pages from @ gulfiliz/gulfiliz.github.io@8c9d897627009dd6cef4bdf918dbe20944045f79 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -172,7 +172,7 @@ ninja.data = [{
         },
       },{id: "post-bell-39-s-theorem-the-most-profound-discovery-in-the-history-of-physics",
         
-          title: "Bell&#39;s Theorem — The Most Profound Discovery in the History of Physics",
+          title: "Bell&#39;s Theorem: The Most Profound Discovery in the History of Physics",
         
         description: "In 1964, John Bell proved that no theory of local hidden variables can reproduce quantum mechanics. In 1982, Aspect confirmed it experimentally. The universe is nonlocal. This changes everything.",
         section: "Posts",
