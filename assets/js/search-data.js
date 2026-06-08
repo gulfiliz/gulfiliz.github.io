b5b7f8bfb2bb274bd37c9d3617f3fcae86334f58
gulfiliz <54915332+gulfiliz@users.noreply.github.com>
2026-06-08 03:43:54 +0000

    Deploying to gh-pages from @ gulfiliz/gulfiliz.github.io@8bfb64111fd2ecbe707713f557a688417355fe26 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -82,11 +82,11 @@ ninja.data = [{
             window.location.href = "/blog/2026/seneca/";
           
         },
-      },{id: "post-logical-operators-code-geometry",
+      },{id: "post-logical-operators-code-degeneracy-and-the-geometry-of-quantum-codes",
         
-          title: "Logical Operators Code Geometry",
+          title: "Logical Operators, Code Degeneracy, and the Geometry of Quantum Codes",
         
-        description: "",
+        description: "The deep structure of quantum error correcting codes, the symplectic geometry of Pauli space, the algebraic definition of logical operators, code degeneracy, and why the distance of a code is a geometric quantity.",
         section: "Posts",
         handler: () => {
           
